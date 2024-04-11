c8a878ba591e4e7ed81979d90edc1c550dcc177f
mdequanter <dequanter@gmail.com>
2024-04-11 10:56:10 +0200

    Update brain.ino

--- brain/brain.ino
+++ brain/brain.ino
@@ -1,45 +1,34 @@
 /**
-This Arduino script controls a NeoPixel ring to simulate the activity of different brain zones. 
-It implements a gradual transition between different colors for the parietal, frontal, and temporal zones of the brain, creating a smooth and visually engaging representation of brain activity. 
-The script defines functions to set pixels with gradual color transitions, allowing for customizable and dynamic lighting patterns reflecting the simulated brain activity.
-
+This code controls a NeoPixel strip to simulate the activity of different brain zones. 
+It listens for serial commands ('f' for frontal, 't' for temporal, 'p' for parietal) and changes the color of the corresponding LED zone gradually, 
+creating a smooth transition effect. 
+The loop continuously checks for incoming serial commands and executes the corresponding action based on the received command.
 **/
 
 
+
+
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
-int pariental[2] = {0, 7};
-int temporalLeft[2] = {8, 15};
-int temporalRight[2] = {16, 23};
-int temporal[2] = {8, 23};
-int frontal[2] = {24, 31};
+// Define the pins for the NeoPixels
+#define PIN        D6
+#define NUMPIXELS 32
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        D6 // On Trinket or Gemma, suggest changing this to 1
+// Define the colors for each brain zone
+#define FRONTAL_COLOR pixels.Color(0, 0, 255)
+#define TEMPORAL_COLOR pixels.Color(0, 255, 0)
+#define PARIETAL_COLOR pixels.Color(255, 0, 0)
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 32 // Popular NeoPixel ring size
-
-// When setting up the NeoPixel library, we tell it how many pixels,
-// and which pin to use to send signals. Note that for older NeoPixel
-// strips you might need to change the third parameter -- see the
-// strandtest example for more information on possible values.
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
+// Define the pixel ranges for each brain zone
+int frontal[2] = {24, 31};
+int temporal[2] = {8, 23};
+int parietal[2] = {0, 7};
 
 void setup() {
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
-
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.begin();
+  Serial.begin(9600); // Initialize serial communication
 }
 
 void setPixelsFade(int pixelPos[2], uint32_t startColor, uint32_t endColor, int transitionTime) {
@@ -63,10 +52,21 @@ void setPixelsFade(int pixelPos[2], uint32_t startColor, uint32_t endColor, int 
 }
 
 void loop() {
-  setPixelsFade(frontal, pixels.Color(0, 0, 255), pixels.Color(0, 0, 50), DELAYVAL);
-  delay(1000);
-  setPixelsFade(temporal, pixels.Color(0, 255, 0), pixels.Color(0, 50, 0), DELAYVAL);
-  delay(1000);
-  setPixelsFade(pariental, pixels.Color(255, 0, 0), pixels.Color(50, 0, 0), DELAYVAL);
-  delay(1000);
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    switch (command) {
+      case 'f': // Frontal zone
+        setPixelsFade(frontal, pixels.Color(0, 0, 255), pixels.Color(0, 0, 50), 50);
+        break;
+      case 't': // Temporal zone
+        setPixelsFade(temporal, pixels.Color(0, 255, 0), pixels.Color(0, 50, 0), 50);
+        break;
+      case 'p': // Parietal zone
+        setPixelsFade(parietal, pixels.Color(255, 0, 0), pixels.Color(50, 0, 0), 50);
+        break;
+      default:
+        // Invalid command
+        break;
+    }
+  }
 }
